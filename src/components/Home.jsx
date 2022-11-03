42ae95924002574899e58806372a51e2f98eaefc
Li-Chih Yen <lichih.yen@gmail.com>
2022-11-03 14:10:03 -0400

    done the Home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Li-Chih Yen</h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Full Stack Developer.</h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          A Full-Stack Web Developer who is a logical thinker with a love of resolving issues. Feel excited about making
+          Full-Stack Web Developer who is a logical thinker with a love of resolving issues. Feel excited about making
           products that change people's lives - hopefully in a good way 😊. Be able to collaborate with a team yet work
           independently with diligent attention to detail.
         </p>
