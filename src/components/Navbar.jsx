27e7e70740aaf0171f636faa2b1c7aa944ceaca7
Li-Chih Yen <lichih.yen@gmail.com>
2022-11-03 10:07:24 -0400

    finished the NavBar part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 // import Logo from '../assets/Logo_ly.png'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false)
+  const handleClick = () => setMenu(!menu)
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
@@ -12,30 +15,35 @@ const Navbar = () => {
       </div>
 
       {/* Menu */}
-      <div>
-        <ul className="flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Works</li>
-          <li>Contact</li>
-        </ul>
-      </div>
 
-      {/* Hamburger */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-      {/* Mobile Menu */}
-      <ul className="hidden">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Works</li>
         <li>Contact</li>
       </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!menu ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !menu
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Works</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
       {/*  Socila Icons */}
-      <div></div>
+      <div className="hidden"></div>
     </div>
   )
 }
